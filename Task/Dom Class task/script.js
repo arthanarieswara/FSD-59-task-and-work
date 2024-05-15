8f97ba4c843816654cf396839d1859e74532b0ca
artanarieswara <arthanarieswara44@gmail.com>
2024-05-15 19:58:41 +0530

    added the file in the HTML

--- Task/Dom Class task/script.js
+++ Task/Dom Class task/script.js
@@ -52,19 +52,40 @@ document.body.appendChild(input_create('input','type','text','id','location'));
 document.body.appendChild(break_create());
 
 var btn = document.createElement('button');
+document.body.appendChild(break_create());
 btn.innerHTML = 'Submit';
 document.body.appendChild(btn);
 
+document.body.appendChild(break_create());
+
 btn.onclick = function(){
+    document.body.appendChild(break_create());
     var fName = document.getElementById('firstName').value;
     console.log("First Name:", fName);
+    var fn = document.createElement('div');
+    fn.innerHTML = "First Name: " + fName;
+    document.body.appendChild(fn);
+    document.body.appendChild(break_create());
     var lName = document.getElementById('lastName').value;
     console.log("Last Name:", lName);
+    var ln = document.createElement('div');
+    ln.innerHTML = "Last Name: " + lName;
+    document.body.appendChild(ln);
+    document.body.appendChild(break_create());
     var email = document.getElementById('email').value;
     console.log("Email:", email);
+    var em = document.createElement('div');
+    em.innerHTML = "Email: " + email;
+    document.body.appendChild(em);
+    document.body.appendChild(break_create());
     var location = document.getElementById('location').value;
     console.log("Location:", location);
+    var loc = document.createElement('div');
+    loc.innerHTML = "Location: " + location;
+    document.body.appendChild(loc);
+    document.body.appendChild(break_create());
 }
 
+// displaying the above recived information in the html page
 
-
+                                    
